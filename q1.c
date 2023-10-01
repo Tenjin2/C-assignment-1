25a82d5ed398c1ea9111ba7bb6f9c62f4277daa0
Dhruv Thakur <tdhruv693@gmail.com>
2023-10-01 22:23:26 +0530

    good

---
+++ q1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    float r,a,c;
+    const float pi=3.14;
+    printf("radius of circle is :");
+    scanf("%f",&r);
+    c=2*pi*r;
+    a=pi*r*r;
+    printf("circumference of circle :%f",c);
+    printf("area of circle :%f",a);
+}
+
