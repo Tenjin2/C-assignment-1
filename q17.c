348f0141946cbe2f3cbde9bcda6e2ddffde3af88
Dhruv Thakur <tdhruv693@gmail.com>
2023-10-02 14:07:57 +0530

    q17.c

---
+++ q17.c
@@ -0,0 +1,14 @@
+//leap year program with logic explanation and output
+ #include <stdio.h>
+ int main(){
+    int year;
+    printf("enter a year :");
+    scanf("%d",&year);
+    if(year%4==0){
+        printf("it is a leap year");
+    }
+    else{
+        printf("it is not a leap year");
+    }
+    return 0;
+ }
