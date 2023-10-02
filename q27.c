0bd635cba81290b706a709f5d0cc432414349341
Dhruv Thakur <tdhruv693@gmail.com>
2023-10-02 20:30:31 +0530

    q27.c

---
+++ q27.c
@@ -0,0 +1,25 @@
+//palindrome program using while loop
+#include <stdio.h>
+int main(){
+    int a,b,c,p,sum=1;
+    printf("write number :");
+    scanf("%d",&a);
+    p=a;
+    b=a%10;
+    c=b*10;
+    a=a/10;
+    while(a>0){
+        b=a%10;
+        c=c+b;
+        c=c*10;
+        a=a/10;
+    }
+    c=c/10;
+    if (c==p){
+        printf("it is a palindrome number");
+    }
+    else{
+        printf("it is not a palindrome number");
+    }
+    return 0;
+}
