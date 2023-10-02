057ecd1eeed39f8db574b0df1d2242388aa95f89
Dhruv Thakur <tdhruv693@gmail.com>
2023-10-02 13:56:48 +0530

    q15.c

---
+++ q15.c
@@ -0,0 +1,14 @@
+//program for finding greater between 2 numbers
+ #include <stdio.h>
+ int main(){
+    int a,b;
+    printf("2 numbers are :");
+    scanf("%d %d",&a,&b);
+    if (a>b){
+        printf("%d is greater",a);
+    }
+    else{
+        printf("%d is greater",b);
+    }
+    return 0;
+ }
