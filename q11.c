c06dfd3b478010d1257f3b5ea34db0c5767f8699
Dhruv Thakur <tdhruv693@gmail.com>
2023-10-02 13:21:30 +0530

    q11

---
+++ q11.c
@@ -0,0 +1,9 @@
+// Program to Read Integer and Print First Three Powers
+ #include <stdio.h>
+ int main(){
+    int n;
+    printf("give integer :");
+    scanf("%d",&n);
+    printf("1st-%d 2nd-%d 3rd-%d",n,n*n,n*n*n);
+    return 0;
+ }
